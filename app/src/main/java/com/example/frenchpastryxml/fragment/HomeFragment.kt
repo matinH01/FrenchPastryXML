2a5f52dcf08e0e7e5aa417056c3ef3eae345bb82
matinH01 <matinhamdolahi117@gmail.com>
2025-10-05 13:18:16 +0330

    Create HomeFragment

---
+++ app/src/main/java/com/example/frenchpastryxml/fragment/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.frenchpastryxml.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
