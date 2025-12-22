08ca409ec2eaaf7069442885be56c9346b6d0d8c
matinH01 <matinhamdolahi117@gmail.com>
2025-12-22 23:35:14 +0330

    Create ProfileFragment.kt

---
+++ app/src/main/java/com/example/frenchpastryxml/fragment/ProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.example.frenchpastryxml.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
